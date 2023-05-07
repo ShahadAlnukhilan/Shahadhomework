0774eda0ef1cb7506b5d4ca32a2a9198361ff3b9
Shahad Alnukhilan <shahadalnukhilan@Shahads-MacBook-Air.local>
2023-05-07 23:00:30 +0300

    Shahad Homewoek1

---
+++ معسكر طويق/MyPlayground.playground/Contents.swift
@@ -0,0 +1,56 @@
+import UIKit /*
+var greeting = "Hello, playground"
+
+var dec1 : Double = 1.2
+var dec2 : Float = 2.1
+var dec3 : Double = dec1 + Double(dec2)
+print(dec3)
+
+
+var isloacked = true
+var  isAuthentication : Bool
+var xyz : Character = "A"
+var 😀 = "ABC"
+print(😀)
+var instruction : String = """
+- login
+- verify
+- bla bla
+"""
+print(instruction)
+var x1 = 10
+var x2 = 20
+var x3 = x1 + x2
+var x4 = x1 - x2
+var x5 = (10 + 20) * 5 / 3
+
+print(x5)
+var x = 10
+x+=5
+print(x)
+
+var result1 = true
+var result2 = false
+ // اخزنها في متغير
+print(result1 && result2)
+print(result1 || result2)
+print(result1 != result2)
+
+
+if (10 > 5){
+    print("")
+}
+else{
+    
+} */
+
+
+let username = "Shahad"
+let password  = "Shahad@1212"
+if ( username == "Shahad" && password == "Shahad@1212" )  {
+    print("successfully logged In.")
+}
+else{
+    
+    print("erorr")
+}
