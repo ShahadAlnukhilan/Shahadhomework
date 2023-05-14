d69d7a23986fb3ac9b45d9f4f96af6f39b39c707
Classes <shahadalnukhilan@Shahads-MacBook-Air.local>
2023-05-15 00:34:03 +0300

    first commit

---
+++ FirstLab/FirstLab/FirstLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstLabApp.swift
+//  FirstLab
+//
+//  Created by Shahad Alnukhilan on 24/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct FirstLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
